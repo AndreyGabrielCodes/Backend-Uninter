d89ce7a75c1b6c6427f3f5c1aceca348c88f72da
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-08-18 21:37:59 -0300

    Importação projeto Maven

---
+++ backendrest/src/main/java/com/uninter/backendrest/BackendrestApplication.java
@@ -0,0 +1,13 @@
+package com.uninter.backendrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendrestApplication.class, args);
+	}
+
+}
---
+++ backendrest/src/test/java/com/uninter/backendrest/BackendrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uninter.backendrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
