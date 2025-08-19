c5ca128d68be787b36c187016c837fefa4c98f6b
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-08-18 22:15:23 -0300

    Retirado Lombok

--- backendrest/src/main/java/com/uninter/backendrest/model/Tarefa.java
+++ backendrest/src/main/java/com/uninter/backendrest/model/Tarefa.java
@@ -7,13 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Entity
 public class Tarefa {
 
@@ -24,5 +18,51 @@ public class Tarefa {
 	private String nome;
 	private LocalDate dataEntrega;
 	private String responsavel;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+	public String getResponsavel() {
+		return responsavel;
+	}
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+	@Override
+	public String toString() {
+		return "Tarefa [id=" + id + ", nome=" + nome + ", dataEntrega=" + dataEntrega + ", responsavel=" + responsavel
+				+ "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(dataEntrega, id, nome, responsavel);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tarefa other = (Tarefa) obj;
+		return Objects.equals(dataEntrega, other.dataEntrega) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome) && Objects.equals(responsavel, other.responsavel);
+	}
 	
 }
